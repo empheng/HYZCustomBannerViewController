d199030545ae5545df73911fb8d8a571f8fe8a9f
emp.heng <yongzhengheng@anjuke.com>
2015-09-07 14:16:31 +0800

    Initial Commit

---
+++ HYZCustomBannerViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYZCustomBannerViewController
+//
+//  Created by emp.heng on 15/9/7.
+//  Copyright (c) 2015年 emp.heng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HYZCustomBannerViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HYZCustomBannerViewController
+//
+//  Created by emp.heng on 15/9/7.
+//  Copyright (c) 2015年 emp.heng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
